eaee61f03082a82edb0364b41ff5591381c717cc
KailasVS666 <sharjikailas@gmail.com>
2026-04-16 15:22:53 +0530

    Add GET books API endpoint

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
