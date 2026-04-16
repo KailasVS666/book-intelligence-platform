8db9bc1615780751615cd36b72110102954bd7e1
KailasVS666 <sharjikailas@gmail.com>
2026-04-16 15:12:27 +0530

    Add Book model

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    rating = models.FloatField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
